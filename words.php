0521782a9fbe953c6ed2e6a0b8b6f1b7d9922922
Aleksey Khozin <ahozin@hotmail.com>
2019-03-02 18:53:56 +0300

    ru-sites semantics

--- words.php
+++ words.php
@@ -1,10 +1,14 @@
 <?php
     $result = '[]';
-    if (isset($_POST['count']) && is_numeric($_POST['count'])) {
+    if (isset($_POST['type']) && isset($_POST['count']) && is_string($_POST['type']) && is_numeric($_POST['count'])) {
+        $type = (string)$_POST['type'];
         $count = (int)$_POST['count'] + 1;
-        $filename = getcwd() . '/words/words3d-' . $count . '.json';
-        if (file_exists($filename)) {
-            $result = file_get_contents($filename);
+        $types = ['words', 'sites'];
+        if (in_array($type, $types)) {
+            $filename = getcwd() . '/' . $type . '/' . $type .'3d-' . $count . '.json';
+            if (file_exists($filename)) {
+                $result = file_get_contents($filename);
+            }
         }
     }
     echo $result;
