e033f06d26c80e89b54f55b0364af191d946b7b3
Aleksey Khozin <ahozin@hotmail.com>
2019-03-20 11:39:42 +0300

    sketches of CMS

--- site/cms/js/cms.js
+++ site/cms/js/cms.js
@@ -8,20 +8,51 @@ function collapse(btn, id) {
     btn.innerHTML = '[+]';
   }
 }
-function createMeta(meta) {
-
+function createMetaValue(key, value) {
+  // TODO: encoding strings
+  return `<input type="text" class="key" value="${key}" />=<input type="text" class="value" value="${value}" />`;
 }
-function loadSiteData(data) {
+function createMetaRow(meta) {
+  if (meta.values.length > 0) {
+    var values = [];
+    for(var i = 0; i < meta.values.length; i++) {
+      if (meta.values[i].length == 2) {
+        values.push(createMetaValue(meta.values[i][0], meta.values[i][1]));
+      }
+    }
+    if (values.length > 0) {
+      var row = `<div class="list-row"><button type="button" class="simplebutton">&#10005;</button>`;
+      for(var i = 0; i < values.length; i++) {
+        row += values[i];
+      }
+      row += `<button type="button" class="simplebutton"${values.length == 1 ? `disabled="disabled"` : ""} style="margin-left: 0.2em">&#9003;</button><button type="button" class="simplebutton last" style="margin-left: 0.2em">&#10151;</button></div>`;
+      return row;
+    }
+  }
+  return "";
+}
+var button_add_new_meta = `<div class="list-row"><button type="button" class="simplebutton" style="font-weight: bold">&#8617;</button></div>`;
+function updateSiteData(data) {
   console.info(data);
 
-  document.getElementById("newsite").style.display = data.site.new ? "inline-block" : "none";
-  document.getElementById("siteicon-input").value = data.site.icon ? data.site.icon.href : "";
-  document.getElementById("siteicon-img").src = data.site.icon ? data.site.icon.href : "";
-  var sitemeta = document.getElementById("chapter-site-meta");
-  var addmetabutton = sitemeta.lastChild.cloneNode(true);
-  sitemeta.innerHTML = "";
-  sitemeta.appendChild(addmetabutton.cloneNode(true));
-  for(var i = 0; i < data.site.meta.length; i++) {
-    console.info(data.site.meta[i]);
+  if (data.site.new) {
+    document.getElementById("newsite").style.display = "inline-block";
+    document.getElementById("siteicon-input").value = "";
+    document.getElementById("siteicon-input").readOnly = true;
+    document.getElementById("siteicon-browse").disabled = true;
+    document.getElementById("siteicon-img").src = "";
+    document.getElementById("chapter-site-meta").innerHTML = "";        
+  } else {
+    document.getElementById("newsite").style.display = "none";
+    document.getElementById("siteicon-input").value = data.site.icon;
+    document.getElementById("siteicon-input").readOnly = false;
+    document.getElementById("siteicon-browse").disabled = false;
+    document.getElementById("siteicon-img").src = data.site.icon;
+    var sitemeta = "";
+    for(var i = 0; i < data.site.meta.length; i++) {
+      sitemeta += createMetaRow(data.site.meta[i]);
+    }
+    sitemeta += button_add_new_meta;
+    document.getElementById("chapter-site-meta").innerHTML = sitemeta;
   }
 }
